93f1a8e26fe24fa80f888ee7acb65c690241bdf2
Artem Ivanko <ivankoartem@gmail.com>
2020-09-21 18:37:52 +0300

    init db

---
+++ denta-med-crm/Model/Client.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace denta_med_crm.Model
+{
+    [Serializable]
+    class Client
+    {
+        [JsonProperty("first_name")]
+        public string FirstName;
+        [JsonProperty("middle_name")]
+        public string MiddleName;
+        [JsonProperty("last_name")]
+        public string LastName;
+        //подсветить если сегодня его день рождения или в пределах 7 дней в карточке
+        [JsonProperty("date_of_birth")]
+        public DateTime DateOfBirth;
+        //Типа он с нами c такого-то (сколько-то в карточке)
+        [JsonProperty("first_visit")]
+        public DateTime FirstVisit;
+        //Типа он был у нас последний раз ....
+        [JsonProperty("last_visit")]
+        public DateTime LastVisit;
+        //0 - нет, если есть то сумма скидки в % вводится
+        [JsonProperty("discount")]
+        public int Discount;
+        //основной
+        [JsonProperty("main_phone_number")]
+        public string MainPhoneNumber;
+        //альтернативный
+        [JsonProperty("alternative_phone_number")]
+        public string AlternativePhoneNumber;
+        //список процедур открывается в отдельном окне
+        [JsonProperty("procedures")]
+        public List<Procedure> Procedures;
+    }
+}
---
+++ denta-med-crm/Model/Procedure.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace denta_med_crm.Model
+{
+    [Serializable]
+    struct Procedure
+    {
+        //Авто подсказка по первым буквам (поиск по всей строке любых процедур которые когда либо и кому либо были проведены, при поиске приведение к одному регистру)
+        [JsonProperty("procedure_name")]
+        public string ProcedureName;
+        //заметка доктора любая
+        [JsonProperty("procedure_note")]
+        public string ProcedureNote;
+        //до какого гарантия, автоматически выставлять +1 год от текущей даты
+        [JsonProperty("warranty_period")]
+        public DateTime WarrantyPeriod;
+        //по умолчанию текущая дата может быть будующей датой (записан пациент на какое-то число)
+        [JsonProperty("procedure_date")]
+        public DateTime ProcedureDate;
+        //Выполнена или нет
+        [JsonProperty("completed")]
+        public bool Сompleted;
+        //Авто подсказка по первым буквам (поиск по всей строке прошлых докторов которые когда либо и кому либо были указаны, при поиске приведение к одному регистру)
+        [JsonProperty("doctor")]
+        public string Doctor;
+    }
+}
